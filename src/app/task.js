b124bdff26b8191c0ea1b331b5ebc441c68dc456
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-10-21 00:23:58 +0200

    add task class

---
+++ src/app/task.js
@@ -0,0 +1,16 @@
+
+class Task {
+  constructor(title, description, dueDate, priority) {
+    this.id=Date.now();
+    this.title=title;
+    this.description=description;
+    this.dueDate=dueDate;
+    this.priority=priority;
+    this.completed=false;
+  }
+
+  function add(params) {
+    
+  }
+
+}
