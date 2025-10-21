8d8b69caa3770839de0b25def2016e2d48dfe05d
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-10-21 18:01:25 +0200

    add functionality to adding projects

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import "./css/style.css";
-import Task from './classes/Task.js'
-import Task from './classes/Project.js'
+import Task from "./classes/Task.js";
+import Project from "./classes/Project.js";
+
+const addProjectBtn = document.querySelector("#addProject");
+const projectsContainer = document.querySelector(".projects");
+
+if (!localStorage.getItem("projects")) {
+  saveDefaultProjects();
+}
+
+let projects = loadProjectsFromStorage();
+renderProjects();
 
 function saveProjectsToStorage(projects) {
   if (projects instanceof Array) {
@@ -13,9 +23,9 @@ function saveProjectsToStorage(projects) {
 
 function saveDefaultProjects() {
   // DEFAULT PROJECTS
-  const Inbox = new Project("Inbox");
-  const Today = new Project("Today");
-  const ThisWeek = new Project("ThisWeek");
+  const Inbox = new Project("Inbox", true);
+  const Today = new Project("Today", true);
+  const ThisWeek = new Project("ThisWeek", true);
 
   const projects = [Inbox, Today, ThisWeek];
 
@@ -23,10 +33,9 @@ function saveDefaultProjects() {
 }
 
 function loadProjectsFromStorage() {
-  saveDefaultProjects();
   let parsedProjects = JSON.parse(localStorage.getItem("projects")) ?? [];
   const projects = parsedProjects.map((project) => {
-    const p = new Project(project.name);
+    const p = new Project(project.name, project.isDefault ?? false);
     p.id = project.id;
     p.tasks = project.tasks.map((t) => {
       const task = new Task(
@@ -41,5 +50,42 @@ function loadProjectsFromStorage() {
     });
     return p;
   });
+
+  return projects;
 }
 
+function renderProjects() {
+  projectsContainer.innerHTML = "";
+  projects
+    .filter((project) => !project.isDefault)
+    .forEach((project) => {
+      const btnProject = `<button data-id="${project.id}">${project.name} </button>`;
+      projectsContainer.insertAdjacentHTML("beforeend", btnProject);
+    });
+}
+
+addProjectBtn.addEventListener("click", () => {
+  if (!document.querySelector(".new-project-wrapper")) {
+    const input = `<input type="text" class="new-project-input" placeholder="project name">`;
+    const button = `<button class="confirm-add">Add</button>`;
+    const wrapper = `<div class='flex new-project-wrapper'> ${input} ${button} </div>`;
+    projectsContainer.insertAdjacentHTML("beforeend", wrapper);
+  }
+});
+
+projectsContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("confirm-add")) {
+    const input = document.querySelector(".new-project-input");
+    const projectName = input.value.trim();
+
+    if (projectName) {
+      const newProject = new Project(projectName);
+      projects.push(newProject);
+      saveProjectsToStorage(projects);
+      console.log(projects);
+      document.querySelector(".new-project-wrapper").remove();
+
+      renderProjects();
+    }
+  }
+});
