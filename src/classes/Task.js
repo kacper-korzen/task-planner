ed0c01dde79ed09a3403fe0a368983fa4e935d86
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-10-30 16:58:40 +0100

    fix constructor for creating uniqe id

--- src/classes/Task.js
+++ src/classes/Task.js
@@ -1,6 +1,6 @@
 export default class Task {
   constructor(title, description, dueDate, priority) {
-    this.id = Date.now();
+    this.id = crypto.randomUUID();
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
